127a475ef6f1f26f290954873b051816bf3bffc4
mrbean27123 <mrbean27123@gmail.com>
2025-06-17 14:29:43 +0300

    try add support  win7

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
 import tailwindcss from "@tailwindcss/vite"
+import postcssPresetEnv from "postcss-preset-env"
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
+  css: {
+    postcss: {
+      plugins: [
+        postcssPresetEnv({
+          stage: 3,
+          features: {
+            "custom-properties": {
+              preserve: false,
+            },
+          },
+        }),
+      ],
+    },
+  },
+
+  build: {
+    cssTarget: "chrome109",
+  },
 })
