0e276ff4fc7ffb36e7a4bee8aed9cb7366933920
mrbean27123 <mrbean27123@gmail.com>
2025-06-17 14:33:49 +0300

    fix for win7

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     postcss: {
       plugins: [
         postcssPresetEnv({
-          stage: 3,
+          stage: 0,
           features: {
             "custom-properties": {
               preserve: false,
